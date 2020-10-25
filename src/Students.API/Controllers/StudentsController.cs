03a45755c7c191f45eae8515ea317b59918eeb3b
airatzt <airatzt@mail.ru>
2020-10-25 13:26:14 +0300

    Add student api

---
+++ src/Students.API/Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿namespace Students.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Students.API.Models;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+
+        public ActionResult<Student> StudentById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            return new Student
+            {
+                Id = 1,
+                Sex = 1,
+                Surname = "Paul",
+                Name = "Phil",
+                Patronymic = "Alex",
+                Slug = "sniper"
+            };
+        }
+    }
+}
---
+++ src/Students.API/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace Students.API.Models
+{
+    public class Student
+    {
+        /// <summary>
+        /// Это поле системное, изменение данного поля не допускается.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Пол (обязательное поле)
+        /// https://en.wikipedia.org/wiki/ISO/IEC_5218
+        /// </summary>
+        public byte Sex { get; set; }
+        /// <summary>
+        /// Фамилия (обязательное, максимальная длина 40 символов)
+        /// </summary>
+        public string Surname { get; set; }
+        /// <summary>
+        /// Имя (обязательное, максимальная длина 40 символов)
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Отчество ( не обязательное, максимальная длина 60 символов)
+        /// </summary>
+        public string Patronymic { get; set; }
+        /// <summary>
+        /// Уникальный идентификатор студента (not required, должен быть уникальным в рамках всех студентов,
+        /// минимальная длина 6 символов, максимальная длина 16). 
+        /// Опциональный, например мы хотим задать для студента позывной,
+        /// но не хотим чтобы два студента были с одинаковыми позывными
+        /// </summary>
+        public string Slug { get; set; }
+    }
+}
